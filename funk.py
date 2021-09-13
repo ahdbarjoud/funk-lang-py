03acd3aa8e33e8b01dfcf738f34faef37a467446
ahdbarjoud <ahdbarjoud78@gmail.com>
2021-09-13 10:06:03 -0700

    Basic parser works. It can parse expressions while maintaining order of operations.

--- funk.py
+++ funk.py
@@ -20,6 +20,15 @@ class BinaryOperator:
 
   __repr__ = __str__
 
+class UnaryOperator:
+  def __init__(self, operator, value):
+    self.operator, self.value = operator, value
+
+  def __str__(self):
+    return f"{{ 'type': 'UnaryOperator', 'operator': '{self.operator}', 'value': '{self.value}' }}"
+
+  __repr__ = __str__
+
 path = './test.fk'
 
 code = open(path).read()
@@ -240,3 +249,85 @@ class Lexer:
       "position": start_pos,
       "line": self.line
     })
+
+class Parser:
+  def __init__(self, tokens):
+    self.tokens = tokens
+    self.pos = 0
+    self.current_token = self.tokens[self.pos]
+    self.next_token = self.tokens[self.pos+1]
+    self.tokens_length = len(self.tokens) - 1
+    self.program = []
+
+  def next(self):
+    # We increment.
+    self.pos += 1
+    if self.pos > self.tokens_length: # We no go too far.
+      self.current_token = None
+      self.next_token = None
+      return
+
+    self.current_token = self.tokens[self.pos] # We set current token.
+
+    if self.pos+1 > self.tokens_length:
+      self.next_token = None
+    else:
+      self.next_token = self.tokens[self.pos + 1] # We set next token.
+
+  def parse(self):
+    while self.current_token != None:
+      if self.current_token["type"] != "keyword":
+
+        self.program.append(self.parse_expr())
+
+      self.next()
+
+  def parse_expr(self):
+    result = self.parse_term()
+
+    while self.current_token != None and self.current_token["type"] == "operator" and self.current_token["value"] in ("+", "-"):
+      op = self.current_token["value"]
+      self.next()
+      result = BinaryOperator(op, result, self.parse_expr())
+
+    return result\
+
+  def parse_term(self):
+    result = self.parse_factor()
+    while self.current_token != None and self.current_token["type"] == "operator" and self.current_token["value"] in ("*", "/"):
+      op = self.current_token["value"]
+      self.next()
+      result = BinaryOperator(op, result, self.parse_factor())
+
+    return result
+
+  def parse_factor(self):
+    fac = self.current_token
+    if self.current_token["type"] == "lpar":
+      self.next()
+      result = self.parse_expr()
+      if self.current_token["type"] != "rpar":
+        raise Exception(f"Missing closing parenthesis on line {self.current_token['line']}")
+      self.next()
+      return result
+
+    elif self.current_token["type"] == "number":
+      n = self.current_token
+      self.next()
+      return n
+
+    elif self.current_token["type"] == "operator" and self.current_token["value"] in ("++","--"):
+      n = self.current_token
+      self.next()
+      return UnaryOperator(n["value"], self.parse_factor())
+
+lex = Lexer(code)
+lex.parse()
+parser = Parser(lex.tokens)
+parser.parse()
+
+# for i in lex.tokens:
+  # print(i)
+
+for i in parser.program:
+  print(i)
