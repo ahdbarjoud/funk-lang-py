3c83d8ec0f4d9935259e1dedcb4668339c40accf
ahdbarjoud <ahdbarjoud78@gmail.com>
2021-11-19 22:42:06 -0800

    // WIP

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,50 @@
-use std::fs;
 use std::env;
+use std::fs;
 
-mod lexer;
-mod structs;
-mod parser;
 mod interpreter;
+mod lexer;
+mod parser;
+mod structs;
+pub use crate::interpreter::interpreter::Interpreter;
 pub use crate::lexer::lexer::Lexer;
 pub use crate::parser::parser::Parser;
-pub use crate::interpreter::interpreter::Interpreter;
 pub use crate::structs::structs::*;
 use std::collections::hash_map::HashMap;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  if args.len() < 2 {
-    panic!("Forgot to mention file to run.");
-  }
-  let filename = &args[1];
-  let code = fs::read_to_string(filename)
-    .expect("Could not read file");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Forgot to mention file to run.");
+    }
+    let filename = &args[1];
+    let code = fs::read_to_string(filename).expect("Could not read file");
 
-  let mut lexer = Lexer{ code: String::new(), last_pos: 0, pos: 0, line: 1, line_pos: 0};
-  let tokens: Vec<Token> = lexer.lex(code);
+    let mut lexer = Lexer {
+        code: String::new(),
+        last_pos: 0,
+        pos: 0,
+        line: 1,
+        line_pos: 0,
+    };
+    let tokens: Vec<Token> = lexer.lex(code);
 
-  let mut parser = Parser{pos: 0, last_pos: tokens.len() - 1, current_token: None, next_token: None, tokens: tokens};
-  let program: Vec<AST> = parser.parse();
+    let mut parser = Parser {
+        pos: 0,
+        last_pos: tokens.len() - 1,
+        current_token: None,
+        next_token: None,
+        tokens: tokens,
+    };
+    let program: Vec<AST> = parser.parse();
 
-  let mut interpreter = Interpreter { pos: 0, next_ast: None, current_ast: None, last_pos: program.len()-1, asts: program, variables: HashMap::default()};
+    let mut interpreter = Interpreter {
+        pos: 0,
+        next_ast: None,
+        current_ast: None,
+        last_pos: program.len() - 1,
+        asts: program,
+        variables: HashMap::default(),
+    };
 
-  interpreter.interpret();
+    interpreter.interpret();
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,198 +1,236 @@
 pub mod structs {
-  use std::ops::*;
+    use std::ops::*;
 
-  pub const OPEARTORS: [&'static str; 28] = [
-    "+", "-", "*", "/", "%", "^",
-    ">", "<", ">=", "<=", "==", "!=",
-    "!", "&", ":", "?", "|", "::", 
-    "#", "&&", "||", "++", "--", "=", 
-    "###", ".", "..", "..."
-  ];
-  pub const KEYWORDS: [&'static str; 18] = [
-    "funk", "while", "for", "when", "if",
-    "elseif", "else", "Integer", "String",
-    "Decimal", "klass", "in", "return",
-    "Boolean", "Array", "Hash", "mut", "Void"
-  ];
+    pub const OPEARTORS: [&'static str; 28] = [
+        "+", "-", "*", "/", "%", "^", ">", "<", ">=", "<=", "==", "!=", "!", "&", ":", "?", "|",
+        "::", "#", "&&", "||", "++", "--", "=", "###", ".", "..", "...",
+    ];
+    pub const KEYWORDS: [&'static str; 18] = [
+        "funk", "while", "for", "when", "if", "elseif", "else", "Integer", "String", "Decimal",
+        "klass", "in", "return", "Boolean", "Array", "Hash", "mut", "Void",
+    ];
 
-  #[derive(Debug, Clone, PartialEq)]
-  pub enum TokenType {
-    LPar,
-    RPar,
-    Number,
-    Newline,
-    LCurl,
-    RCurl,
-    LBrac,
-    RBrac,
-    Comma,
-    Semi,
-    String,
-    Operator,
-    Keyword,
-    Identifier
-  }
-
-  #[derive(Debug, Clone, PartialEq)]
-  pub struct Token {
-    pub typ: TokenType,
-    pub value: String,
-    pub line: usize,
-    pub line_pos: usize
-  }
-
-  #[derive(PartialEq, Debug, Clone)]
-  pub enum AST{
-    BiOpAST{op: String, left: Box<AST>, right: Box<AST>},
-    UnOp{op: String, value: Box<AST>},
-    Integer{value: i64},
-    Decminal{value: f64},
-    Str{value: String},
-    Assgignment{name: String, value: Box<AST>, var_type: String, scope: String, line: usize},
-    FunktionParameter{name: String, typ: String},
-    Funktion{name: String, return_typ: String, params: Vec<Box<AST>>, body: Vec<Box<AST>>},
-    Conditional{typ: String, expr: Option<Box<AST>>, body: Vec<Box<AST>>, other: Option<Box<AST>>},
-    CallItem{name: String, call_type: String, args: Option<Vec<Box<AST>>>, scope: String},
-    Variable{name: String, value: Box<AST>, var_type: String, scope: String, line: usize}
-  }
-
-  impl Add for AST {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self{
-      if let AST::Integer{value} = self {
-        let val1 = value;
-
-        if let AST::Integer{value} = other {
-          let val2 = value;
-          return Self::Integer{value: val1 + val2}
-        } else if let AST::Decminal{value} = other {
-          let val2 = value;
-          let val1 = val1 as f64;
-          return Self::Decminal{value: val1 + val2}
-        } else {
-          panic!("Add AST panic.")
-        }
-      } else if let AST::Decminal{value} = self {
-        let val1 = value;
-
-        if let AST::Decminal{value} = other {
-          let val2 = value;
-          return Self::Decminal{value: val1 + val2}
-        } else if let AST::Integer{value} = other {
-          let val2 = value as f64;
-          return Self::Decminal{value: val1 + val2}
-        } else {
-          panic!("Add AST panic.")
-        }
-      } else {
-        panic!("Add AST panic.")
-      }
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum TokenType {
+        LPar,
+        RPar,
+        Number,
+        Newline,
+        LCurl,
+        RCurl,
+        LBrac,
+        RBrac,
+        Comma,
+        Semi,
+        String,
+        Operator,
+        Keyword,
+        Identifier,
     }
-  }
 
-  impl Sub for AST {
-    type Output = Self;
-
-    fn sub(self, other: Self) -> Self {
-      if let AST::Integer{value} = self {
-        let val1 = value;
-
-        if let AST::Integer{value} = other {
-          let val2 = value;
-          Self::Integer{value: val1 - val2}
-        } else if let AST::Decminal{value} = other {
-          let val2 = value;
-          let val1 = val1 as f64;
-          Self::Decminal{value: val1 - val2}
-        } else {
-          panic!("Sub AST panic.")
-        }
-      } else if let AST::Decminal{value} = self {
-        let val1 = value;
-
-        if let AST::Decminal{value} = other {
-          let val2 = value;
-          Self::Decminal{value: val1 - val2}
-        } else if let AST::Integer{value} = other {
-          let val2 = value as f64;
-          Self::Decminal{value: val1 - val2}
-        } else {
-          panic!("Sub AST panic.")
-        }
-      } else {
-        panic!("Sub AST panic.")
-      }
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct Token {
+        pub typ: TokenType,
+        pub value: String,
+        pub line: usize,
+        pub line_pos: usize,
     }
-  }
 
-  impl Mul for AST {
-    type Output = Self;
-
-    fn mul(self, other: Self) -> Self {
-      if let AST::Integer{value} = self {
-        let val1 = value;
-
-        if let AST::Integer{value} = other {
-          let val2 = value;
-          Self::Integer{value: val1 * val2}
-        } else if let AST::Decminal{value} = other {
-          let val1 = val1 as f64;
-          let val2 = value;
-          Self::Decminal{value: val1 * val2}
-        } else {
-          panic!("Mul AST panic.");
-        }
-      } else if let AST::Decminal{value} = self {
-        let val1 = value;
-
-        if let AST::Integer{value} = other {
-          let val2 = value as f64;
-          Self::Decminal{value: val1 * val2}
-        } else if let AST::Decminal{value} = other {
-          let val2 = value;
-          Self::Decminal{value: val1 * val2}
-        } else {
-          panic!("Mul AST panic.")
-        }
-      } else {
-        panic!("Mul AST panic.");
-      }
+    #[derive(PartialEq, Debug, Clone)]
+    pub enum AST {
+        BiOpAST {
+            op: String,
+            left: Box<AST>,
+            right: Box<AST>,
+        },
+        UnOp {
+            op: String,
+            value: Box<AST>,
+        },
+        Integer {
+            value: i64,
+        },
+        Decminal {
+            value: f64,
+        },
+        Str {
+            value: String,
+        },
+        Assgignment {
+            name: String,
+            value: Box<AST>,
+            var_type: String,
+            scope: String,
+            line: usize,
+        },
+        FunktionParameter {
+            name: String,
+            typ: String,
+        },
+        Funktion {
+            name: String,
+            return_typ: String,
+            params: Vec<Box<AST>>,
+            body: Vec<Box<AST>>,
+        },
+        Conditional {
+            typ: String,
+            expr: Option<Box<AST>>,
+            body: Vec<Box<AST>>,
+            other: Option<Box<AST>>,
+        },
+        CallItem {
+            name: String,
+            call_type: String,
+            args: Option<Vec<Box<AST>>>,
+            scope: String,
+        },
+        Variable {
+            name: String,
+            value: Box<AST>,
+            var_type: String,
+            scope: String,
+            line: usize,
+        },
     }
-  }
 
-  impl Div for AST {
-    type Output = Self;
+    impl Add for AST {
+        type Output = Self;
 
-    fn div(self, other: Self) -> Self {
-      if let AST::Integer{value} = self {
-        let val1 = value;
+        fn add(self, other: Self) -> Self {
+            if let AST::Integer { value } = self {
+                let val1 = value;
 
-        if let AST::Integer{value} = other {
-          let val2 = value;
-          Self::Integer{value: val1 / val2}
-        } else if let AST::Decminal{value} = other {
-          let val1 = val1 as f64;
-          let val2 = value;
-          Self::Decminal{value: val1 / val2}
-        } else {
-          panic!("Div AST panic.");
+                if let AST::Integer { value } = other {
+                    let val2 = value;
+                    return Self::Integer { value: val1 + val2 };
+                } else if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    let val1 = val1 as f64;
+                    return Self::Decminal { value: val1 + val2 };
+                } else {
+                    panic!("Add AST panic.")
+                }
+            } else if let AST::Decminal { value } = self {
+                let val1 = value;
+
+                if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    return Self::Decminal { value: val1 + val2 };
+                } else if let AST::Integer { value } = other {
+                    let val2 = value as f64;
+                    return Self::Decminal { value: val1 + val2 };
+                } else {
+                    panic!("Add AST panic.")
+                }
+            } else {
+                panic!("Add AST panic.")
+            }
         }
-      } else if let AST::Decminal{value} = self {
-        let val1 = value;
-
-        if let AST::Integer{value} = other {
-          let val2 = value as f64;
-          Self::Decminal{value: val1 / val2}
-        } else if let AST::Decminal{value} = other {
-          let val2 = value;
-          Self::Decminal{value: val1 / val2}
-        } else {
-          panic!("Div AST panic.")
-        }
-      } else {
-        panic!("Div AST panic.");
-      }
     }
-  }
+
+    impl Sub for AST {
+        type Output = Self;
+
+        fn sub(self, other: Self) -> Self {
+            if let AST::Integer { value } = self {
+                let val1 = value;
+
+                if let AST::Integer { value } = other {
+                    let val2 = value;
+                    Self::Integer { value: val1 - val2 }
+                } else if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    let val1 = val1 as f64;
+                    Self::Decminal { value: val1 - val2 }
+                } else {
+                    panic!("Sub AST panic.")
+                }
+            } else if let AST::Decminal { value } = self {
+                let val1 = value;
+
+                if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    Self::Decminal { value: val1 - val2 }
+                } else if let AST::Integer { value } = other {
+                    let val2 = value as f64;
+                    Self::Decminal { value: val1 - val2 }
+                } else {
+                    panic!("Sub AST panic.")
+                }
+            } else {
+                panic!("Sub AST panic.")
+            }
+        }
+    }
+
+    impl Mul for AST {
+        type Output = Self;
+
+        fn mul(self, other: Self) -> Self {
+            if let AST::Integer { value } = self {
+                let val1 = value;
+
+                if let AST::Integer { value } = other {
+                    let val2 = value;
+                    Self::Integer { value: val1 * val2 }
+                } else if let AST::Decminal { value } = other {
+                    let val1 = val1 as f64;
+                    let val2 = value;
+                    Self::Decminal { value: val1 * val2 }
+                } else {
+                    panic!("Mul AST panic.");
+                }
+            } else if let AST::Decminal { value } = self {
+                let val1 = value;
+
+                if let AST::Integer { value } = other {
+                    let val2 = value as f64;
+                    Self::Decminal { value: val1 * val2 }
+                } else if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    Self::Decminal { value: val1 * val2 }
+                } else {
+                    panic!("Mul AST panic.")
+                }
+            } else {
+                panic!("Mul AST panic.");
+            }
+        }
+    }
+
+    impl Div for AST {
+        type Output = Self;
+
+        fn div(self, other: Self) -> Self {
+            if let AST::Integer { value } = self {
+                let val1 = value;
+
+                if let AST::Integer { value } = other {
+                    let val2 = value;
+                    Self::Integer { value: val1 / val2 }
+                } else if let AST::Decminal { value } = other {
+                    let val1 = val1 as f64;
+                    let val2 = value;
+                    Self::Decminal { value: val1 / val2 }
+                } else {
+                    panic!("Div AST panic.");
+                }
+            } else if let AST::Decminal { value } = self {
+                let val1 = value;
+
+                if let AST::Integer { value } = other {
+                    let val2 = value as f64;
+                    Self::Decminal { value: val1 / val2 }
+                } else if let AST::Decminal { value } = other {
+                    let val2 = value;
+                    Self::Decminal { value: val1 / val2 }
+                } else {
+                    panic!("Div AST panic.")
+                }
+            } else {
+                panic!("Div AST panic.");
+            }
+        }
+    }
 }
